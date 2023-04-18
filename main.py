25e173cd0a336e68d7f2155c0e4e4372f29d300f
Henrik Loeser <data-henrik@users.noreply.github.com>
2023-04-18 16:06:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	return "<p>Hello World</p>"
+  return "<p>Hello World</p>"
 
 # Get the PORT from environment
-port = os.getenv('PORT', '5000')
+port = os.getenv('PORT', '8080')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=int(port))
